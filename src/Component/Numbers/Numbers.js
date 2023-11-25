29234f7e3ad40528a6a3aa1393a220bc3ecdfa5a
Naveen Raj <nn400724@gmail.com>
2023-11-25 22:40:07 +0530

    second

--- src/Component/Numbers/Numbers.js
+++ src/Component/Numbers/Numbers.js
@@ -5,7 +5,7 @@ function Numbers(){
     return(
         <div className="bg-black  flex flex-col justify-center items-center text-center md:flex-row md:p-8 md:pt-[180px] md:mb-[-80px] lg:mb-[-125px]  lg:p-14  xl:mb-[-200px] xl:p-36 p-6 gap-6 xl:pt-64" id="Numbers">
             <div className="bg-[#1A1A1A] flex flex-col w-full h-32 lg:h-40 rounded-md justify-center md:z-50" data-aos="fade-up" data-aos-duration="1500">
-                <h1 className=" text-[#ffbd39] text-[37px] font-bold">4</h1>
+                <h1 className=" text-[#ffbd39] text-[37px] font-bold">6</h1>
                 <h1  className=" text-white text-[18px]">Projects</h1>
             </div>
 
